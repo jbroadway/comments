7be929ebbdc69963f1720e144bcd5038d1503f29
BronyBorn <minecraftsolutionsinc@gmail.com>
2014-06-11 14:50:23 -0500

    Added missing pgsql installation file

---
+++ conf/install_pgsql.sql
@@ -0,0 +1,10 @@
+create table comments (
+	id serial not null primary key,
+	identifier character(72) not null,
+	"user" integer not null,
+	status integer not null,
+	ts timestamp not null,
+	"comment" text not null
+);
+create index identifier on comments (identifier, status, ts);
+create index "user" on comments ("user", status, ts);
